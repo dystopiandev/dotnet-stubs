71711f12c7ca494db1b934b3fa38962f7b1fc80b
Redhart Azul <me@dystopian.dev>
2022-11-22 20:35:43 +0100

    cleanup

--- src/RabbitmqConsumer/Program.cs
+++
@@ -1,7 +0,0 @@
-using RabbitmqConsumer;
-
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => { services.AddHostedService<AmqpConsumer>(); })
-    .Build();
-
-await host.RunAsync();
--- src/RabbitmqProducer/Program.cs
+++
@@ -1,7 +0,0 @@
-using RabbitmqProducer;
-
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => { services.AddHostedService<AmqpProducer>(); })
-    .Build();
-
-await host.RunAsync();
